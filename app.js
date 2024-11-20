b0608ff5b084f581d4c0c0817843ea412453b8fe
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-11-20 12:46:32 +0200

    normal mongo

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const { MongoClient } = require("mongodb");
-const url = "mongodb+srv://sayedrady61:<db_password>@proj-1.n71gc.mongodb.net/";
+
+const url = "mongodb+srv://sayedrady61:012301050180@courses.b9h82.mongodb.net/";
+
+const client = new MongoClient(url);
+
+const main = async () => {
+  await client.connect();
+  console.log("connected successfully to server");
+  const db = client.db("courses");
+  const collection = db.collection("courses");
+  await collection.insertOne({ title: "new here", price: 333 });
+  const data = await collection.find().toArray();
+  console.table(data);
+};
+main();
